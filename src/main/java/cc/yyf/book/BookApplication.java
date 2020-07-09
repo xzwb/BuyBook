f67c0e995afe56baba3588c4bd81d26a33a3d2f5
xzwb <1478863479@qq.com>
2020-07-09 11:39:21 +0800

    docs(): add a new springBoot poject
    创建了一个新的springBoot项目

---
+++ src/main/java/cc/yyf/book/BookApplication.java
@@ -0,0 +1,13 @@
+package cc.yyf.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookApplication.class, args);
+    }
+
+}
