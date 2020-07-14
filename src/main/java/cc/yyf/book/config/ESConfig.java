1d875569bf3ec281de993808006eb07667b1cc44
xzwb <1478863479@qq.com>
2020-07-14 13:43:01 +0800

    feat(application.peoperties): go cloud server
    配置项目到云服务器

--- src/main/java/cc/yyf/book/config/ESConfig.java
+++ src/main/java/cc/yyf/book/config/ESConfig.java
@@ -16,11 +16,25 @@ public class ESConfig {
      * 配置es高级客户端
      * @return
      */
+//    @Bean
+//    public RestHighLevelClient restHighLevelClient() {
+//        RestHighLevelClient client = new RestHighLevelClient(
+//                RestClient.builder(
+//                        new HttpHost("localhost", 9200, "http")
+//                )
+//        );
+//        return client;
+//    }
+
+    /**
+     * 配置高级客户端 云服务器版
+     * @return
+     */
     @Bean
     public RestHighLevelClient restHighLevelClient() {
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
-                        new HttpHost("localhost", 9200, "http")
+                        new HttpHost("49.234.70.67", 9200, "http")
                 )
         );
         return client;
